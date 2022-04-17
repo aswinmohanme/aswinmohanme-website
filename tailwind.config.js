a74f59629ba9e1e67bb4292bab3cf0d0ebcc4f64
aswinmohanme <dev.aswinmohan@gmail.com>
2022-04-17 10:39:07 +0530

    Set monospace fonts correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['Avenir', 'Arial', 'system-ui', 'sans-serif'],
-        'mono': ['SF Mono', 'SFMono-Regular', 'ui-monospace',
-          'DejaVu Sans Mono', 'Menlo', 'Consolas', 'monospace']
+        'mono': ['Courier', 'monospace']
       },
     },
   },
